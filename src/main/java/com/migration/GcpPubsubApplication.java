45d9f6b5d6818feaf975d44a3f5d0018f1f2b342
Ajit <priyanka.jntuk@gmail.com>
2023-01-19 15:49:09 +0530

    Initial Commits

---
+++ src/main/java/com/migration/GcpPubsubApplication.java
@@ -0,0 +1,13 @@
+package com.migration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpPubsubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpPubsubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/migration/GcpPubsubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.migration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpPubsubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
